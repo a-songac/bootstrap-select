748398ac6cf68f2dbffae81ca44b459ff6df2977
Arnaud Songa-Cote <arnaud@nabupro.com>
2021-12-13 10:41:28 -0500

    make changes for nabu needs

--- js/bootstrap-select.js
+++ js/bootstrap-select.js
@@ -1,3 +1,12 @@
+/*
+ * **************** NABU MODIFICATION START ****************
+ *   1. Added a public method "separator" to allow to modify the multipleSeparator option dynamically
+ *   2. Modified behavior when generating the text of the dropdown
+ *
+ * Look for "NABU MODIFICATION" in the file
+ *
+ * **************** NABU MODIFICATION END ****************
+ */
 (function ($) {
   'use strict';
 
@@ -720,7 +729,11 @@
           iconElement;
 
       if (options.content) {
-        textElement.innerHTML = options.content;
+        // ************ NABU MODIFICATION 2 START ******************
+        // Always use text in dropdown
+        // textElement.innerHTML = options.optionContent;
+        textElement.innerHTML = options.text;
+        // ************ NABU MODIFICATION 2 END ******************
       } else {
         textElement.textContent = options.text;
 
@@ -847,6 +860,7 @@
     this.remove = Selectpicker.prototype.remove;
     this.show = Selectpicker.prototype.show;
     this.hide = Selectpicker.prototype.hide;
+    this.separator = Selectpicker.prototype.changeSeparator;
 
     this.init();
   };
@@ -3074,7 +3088,14 @@
         .removeClass('bs-select-hidden selectpicker');
 
       $(window).off(EVENT_KEY + '.' + this.selectId);
+    },
+    // **************** NABU MODIFICATION 1 START ****************
+    changeSeparator: function(newSeparator) {
+      // change element data instead of options because it has precedence when refreshing the module: see refresh function
+      this.$element.data('multipleSeparator', newSeparator)
+      this.refresh();
     }
+    // **************** NABU MODIFICATION 1 START ****************
   };
 
   // SELECTPICKER PLUGIN DEFINITION
